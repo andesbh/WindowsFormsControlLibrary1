34f9108c00c94cdb60943152677a32339bb75cdb
Anderson <andersonlucascs@yahoo.com.br>
2022-09-30 10:05:05 -0300

    For learning

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsControlLibrary1;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            userControl11.dataGridView1_CellDoubleClickEvent += new DataGridViewCellEventHandler(dataGridView1_CellDoubleClick);
+         }        
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //Your code for the "dataGridView_CellDoubleClick" event
+        }
+    }
+}
---
+++ WindowsFormsControlLibrary1/UserControl1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WindowsFormsControlLibrary1.UserControl1;
+
+namespace WindowsFormsControlLibrary1
+{
+    public partial class UserControl1: UserControl
+    {
+        public event DataGridViewCellEventHandler dataGridView1_CellDoubleClickEvent;
+
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            this.dataGridView1.CellDoubleClick += dataGridView1_CellDoubleClickEvent;
+        }
+
+        private void dataGridView1_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1_CellDoubleClickEvent?.Invoke(this.dataGridView1, e);
+        }
+    }
+}
